0bd9b92c7cfb13780e48cedcf4b677d7e21afa1b
azumontes <103196397+azumontes@users.noreply.github.com>
2022-04-07 10:10:07 -0600

    Add files via upload

---
+++ azucena_crear_un_archivo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace creacion_archivo_Azucena
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //Pedimos nombre del Archivo
+            Console.WriteLine("Ingrese Nombre del Archivo");
+            String Arc = Console.ReadLine();
+
+            //Abrimos o Creamos un Archivo si no existe.
+            Stream Archivo = new FileStream("./" + Arc + ".txt", FileMode.OpenOrCreate);
+
+            //Esbribimos en nuestro Archivo mediante StramWrite
+            StreamWriter Escribir = new StreamWriter(Archivo);
+
+            Console.WriteLine("Ingres texto para escribir en: " + Arc + ".txt");
+            String Texto = Console.ReadLine();
+
+            Escribir.WriteLine(Texto);
+
+            //Cerramos
+            Escribir.Close();
+            Archivo.Close();
+
+            //Abrimos o Creamos un Archivo si no existe.
+            Stream Archivo2 = new FileStream("./" + Arc + ".txt", FileMode.OpenOrCreate);
+            Console.WriteLine("\nLectura:\n");
+
+            //Leemos nuestro archivo mediante StreamReader
+            StreamReader Leer = new StreamReader(Archivo2);
+            Console.WriteLine(Leer.ReadToEnd());
+
+            //Cerramos
+            Leer.Close();
+            Archivo2.Close();
+
+        }
+    }
+}
